2def0ddda7a42152b391666c840385443908a799
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-14 14:18:17 +0530

    long way schema validation

--- product.js
+++ product.js
@@ -10,8 +10,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/shopApp")
 
 
 // this is a normal way of schema
+// const productSchema = new mongoose.Schema({
+//     name: String,
+//     price: Number,
+//     isOnSale: Boolean
+// })
+
+
+// longer way
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    isOnSale: Boolean
-})
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const bike = new Product({
+    name: "Mountain Bike",
+    price: 999,
+    color: "red"
+});
+
+bike.save()
+    .then(data => {
+        console.log("It Worked!!");
+        console.log(data);
+    })
+    .catch(err => {
+        console.log("Oh No Error!!");
+        console.log(err);
+    })
