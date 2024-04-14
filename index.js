716a558eedeb8aad7ed6e198108e8702537f53d1
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-14 12:23:36 +0530

    schema, model, instance

--- index.js
+++ index.js
@@ -10,9 +10,23 @@ mongoose.connect("mongodb://127.0.0.1:27017/movieApp")
 
 
 // Not a schema, this is just an example how it looks like
-{
-    title: "Amadeus",
-    year: 1986,
-    score: 9.2,
-    rating: "R"
-}
+// {
+//     title: "Amadeus",
+//     year: 1986,
+//     score: 9.2,
+//     rating: "R"
+// }
+
+// Schema
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+// model class, with this I can now make new instances of my movie class
+const Movie = mongoose.model("Movie", movieSchema);
+
+// process of that => 
+const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9.2, rating: "R" });
