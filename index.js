0a84d1215e1aa0c6e2deca16ea5d46cead2990b5
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-14 13:02:20 +0530

    find(R - CRUD)

--- index.js
+++ index.js
@@ -29,4 +29,24 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model("Movie", movieSchema);
 
 // process of that => 
-const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9.2, rating: "R" });
+// const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9.2, rating: "R" });
+
+// trying insertMany, usually we don't use this, but trying just to understand, above line commented to try this.
+
+// "C" of CRUD
+// Movie.insertMany([
+//     { title: "Amelie", year: 2001, score: 8.2, rating: "R" },
+//     { title: "Alien", year: 19879, score: 8.1, rating: "R" },
+//     { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+//     { title: "Stand By Me", year: 1986, score: 8.6, rating: "R" },
+//     { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" }
+// ])
+//     .then(data => {
+//         console.log("It Worked!!!")
+//         console.log(data);
+//     })
+
+// commented above because doing find stuff, "R" of CRUD
+
+// .find({}).then(data => console.log(data)); // this should be done in node repl after .load index.js
+
